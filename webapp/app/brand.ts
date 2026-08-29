eed857ae437bb0c39805b0f323253f73f31797ab
Legacy desenvolvedor <103953894+JET-SOUZA@users.noreply.github.com>
2026-08-29 13:07:01 -0300

    Add webapp branding

---
+++ webapp/app/brand.ts
@@ -0,0 +1,2 @@
+export const CHURCH_LOGO='https://res.cloudinary.com/to3hnwdl/image/upload/f_auto,q_auto,w_600/v1787751748/rl3huul7ycwhferq3hiz.jpg';
+export const LEGACY_LOGO='https://res.cloudinary.com/to3hnwdl/image/upload/f_auto,q_auto,w_600/v1787748565/mnbok2ffwwwckhlnoikm.png';
